6a7ea4bdcb1cc559f546533aa58fed3f9f116b86
Denise Osoria <deniseosoria04@gmail.com>
2024-10-02 19:52:43 -0400

    adding block07 workshop

---
+++ Coursework/block07/workshop/code.js
@@ -0,0 +1,17 @@
+/**
+ * * 1) Declare a string variable and assign it to the value :"You have received this message because you have been chosen to open an important vault."
+    2) Declare and assign 3 varlables, one for each number in the combination. 
+    3) Code1 will be using addition
+    4) Code2 will be using substraction
+    5)Code 3 will be using multiplication
+    6)Display the string and the codes to the user
+ */
+
+let message = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
+
+let code1= 6+4
+let code2= 50-10
+let code3= 59-20
+
+alert(message+'\n\n'+code1+' '+code2+' '+code3)
+
