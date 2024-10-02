156d984a6867b4204d20afb89c2bdc9f0f9ebca6
Denise Osoria <deniseosoria04@gmail.com>
2024-10-02 19:57:44 -0400

    update block07

--- Coursework/block07/workshop/code.js
+++ Coursework/block07/workshop/code.js
@@ -7,8 +7,12 @@
     6)Display the string and the codes to the user
  */
 
+
+
+//message pop-up
 let message = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
 
+//combination codes
 let code1= 6+4
 let code2= 50-10
 let code3= 59-20
