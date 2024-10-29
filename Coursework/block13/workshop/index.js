cbe308ae6a95329deb8267712ee1565d12803cc1
Denise Osoria <deniseosoria04@gmail.com>
2024-10-29 15:37:02 -0400

    Update to block13 worshop

--- Coursework/block13/workshop/index.js
+++ Coursework/block13/workshop/index.js
@@ -1,32 +1,52 @@
+//Creates a random number.
 function rand(limit) {
     return Math.round(Math.random() * limit);
 }
 
+//Converts Farenheit to Celsius.
 function convertToCelsius(fahrenheit) {
     return (fahrenheit - 32) * (5 / 9); // Return the converted Celsius value
 }
 
+//Creates a message depending on the Farenheit value.
 function createMessage(fahrenheit) {
+    //Create a celsuis variable to be used in this function.
     const celsius = convertToCelsius(fahrenheit);  // Convert to Celsius
 
-    // Construct the message based on the Fahrenheit value
+    // Construct the message based on the Fahrenheit value.
+
     let message;
-    if (fahrenheit < 65) {
+
+    //This is to make sure the variable is a number.
+    let numFahrenheit = fahrenheit*1;
+
+    if (numFahrenheit < 65) {
         message = `So it feels cold.`;
-    } else if (fahrenheit > 80) {
+    } else if (numFahrenheit > 80) {
         message = `So it feels hot.`;
     } else {
         message = `So it feels just right.`;
     }
     
     
-    return `Result: ${fahrenheit} Fahrenheit is ${celsius.toFixed(2)} Celsius. ${message}`;
+    // return `Result: ${fahrenheit} Fahrenheit is ${celsius.toFixed(2)} Celsius. ${message}`;
+
+    alert("Result: " + fahrenheit + "°F is " + celsius.toFixed(2) + "°C. " + message);
 }
 
-// Add event listener to the button to trigger the prompt and display the result
-document.getElementById('convertButton').addEventListener('click', () => {
-    let fahrenheit = parseFloat(prompt('Enter a Fahrenheit number, we will convert it to Celsius'));
+const fahrenheitPrompt =
+  "Please enter a number. We will convert that temperature from Fahrenheit to Celsius.";
+let fahrenheit = prompt(fahrenheitPrompt);
+createMessage(+fahrenheit);
 
-    // Display the result in the <p> element
-    document.getElementById('result').textContent = createMessage(fahrenheit);
-});
+alert("Let's try that again.");
+fahrenheit = prompt(fahrenheitPrompt);
+createMessage(+fahrenheit);
+
+alert("Let's try some random temperatures.");
+fahrenheit = rand(110);
+createMessage(fahrenheit);
+
+fahrenheit = rand(110);
+createMessage(fahrenheit);
+
